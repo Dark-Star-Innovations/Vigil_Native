2ef730d5664629a9cb2dd57bff4888ebc2e2fb66
Louis Z. <124650795+Bizyboy@users.noreply.github.com>
2026-01-02 04:02:26 -0500

    Add Vigil Configuration Module with settings import

---
+++ root/workdir/vigil/config/__init__.py
@@ -0,0 +1,18 @@
++  1 """Vigil Configuration Module"""
++  2 from .settings import (
++  3     BOT_NAME,
++  4     BOT_TITLE,
++  5     WAKE_WORDS,
++  6     USER_NAMES,
++  7     PRIMARY_USER_NAME,
++  8     OPENAI_API_KEY,
++  9     ANTHROPIC_API_KEY,
++ 10     POE_API_KEY,
++ 11     ELEVENLABS_API_KEY,
++ 12     LLMConfig,
++ 13     VoiceConfig,
++ 14     Paths,
++ 15     ReflectionConfig,
++ 16     MemoryConfig,
++ 17     get_system_prompt,
++ 18 )
